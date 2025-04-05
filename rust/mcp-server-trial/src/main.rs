bb113f9a8c9b5cc95e22192f1df3022aff04ae4f
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2025-04-06 00:43:12 +0900

    chore: impl mcp-server-trial

--- rust/mcp-server-trial/src/main.rs
+++ rust/mcp-server-trial/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use poem_mcpserver::{McpServer, Tools, stdio::stdio, tool::Text};
+use uuid::Uuid;
+
+struct IdGenerator;
+
+#[Tools]
+impl IdGenerator {
+    async fn generate(&self) -> Text<String> {
+        Text(Uuid::new_v4().to_string())
+    }
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    stdio(McpServer::new().tools(IdGenerator)).await
 }
